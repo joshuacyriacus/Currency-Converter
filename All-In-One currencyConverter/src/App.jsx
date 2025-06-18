ce6725c86d691fef04c75829a74ea95e6ceb5a4c
joshuacyriacus <joshuacyriacus8@gmail.com>
2025-06-18 15:08:58 +0100

    correction

--- All-In-One currencyConverter/src/App.jsx
+++ All-In-One currencyConverter/src/App.jsx
@@ -1,7 +1,4 @@
 import Header from './Components/Header'
-import CurencyBox from './Components/CurrencyBox'
-import TryMan from './Components/TryMan'
-import { useState, useEffect } from 'react';
 
 function App() { 
   
